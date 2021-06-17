91583f03cdb2355f64d4ef2355f7d5504f2c2532
Paritosh Raj <paritoshrajiitd@gmail.com>
2021-06-17 15:47:52 +0530

    send message

--- messenger-app/src/components/Compose/index.js
+++ messenger-app/src/components/Compose/index.js
@@ -1,18 +1,81 @@
-import React from 'react';
+import React, {useState} from "react";
+import axios from 'axios';
 import './Compose.css';
 
 export default function Compose(props) {
+  const [messageInput, setMessageInput] = useState('');
+  const resetInput = () => {
+    setMessageInput('');
+  };
+  const sendMessage = data => {
+    console.log(data.variables);
+    axios({
+      url: 'https://messenger-app.hasura.app/v1/graphql',
+      method: 'post',
+      data: {
+        query: `
+        mutation MyMutation($id_receiver: String, $id_sender: String, $text: String) {
+          insert_messages(objects: {id_receiver: $id_receiver, id_sender: $id_sender, text: $text}) {
+            returning {
+              id
+              id_sender
+              send_at
+              text
+            }
+          }
+        }
+          `,
+        variables: data.variables
+      },
+      headers: {
+        'x-hasura-admin-secret': 'cLzWoSwe7ooq2gB67r5bLrTMMDkNU5wjIZ6G7h5MEcXcp8wgPvzPcZPE6hGk3XW8',
+        'content-type': 'application/json'
+      }
+    })
+    .then(response => {
+        console.log(response.data.data.insert_messages.returning);
+        let tempMessages = response.data.data.insert_messages.returning.map(messages => {
+          return {
+            id: messages.id,
+            author: messages.id_sender,
+            message: messages.text,
+            timestamp: messages.sent_at
+          };
+        });
+        props.addMessage(tempMessages);
+        resetInput();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
     return (
-      <div className="compose">
+      <form
+        className="compose"
+        onSubmit={e => {
+          e.preventDefault();
+          sendMessage(
+            {
+              variables: {
+                text: messageInput,
+                id_sender: props.idSender,
+                id_receiver: props.idReceiver
+              }
+            }
+          );
+        }}
+      >
         <input
           type="text"
           className="compose-input"
-          placeholder="Type a message, @name"
+          value = {messageInput}
+          placeholder={"Type a message for "+props.nameReceiver}
+          onChange={e => (setMessageInput(e.target.value))}
         />
 
         {
           props.rightItems
         }
-      </div>
+      </form>
     );
 }
--- messenger-app/src/components/MessageList/index.js
+++ messenger-app/src/components/MessageList/index.js
@@ -126,6 +126,9 @@ export default function MessageList(props) {
       ] */
       // setMessages([...messages, ...tempMessages])
   }
+  const add2Message = newMsg => {
+    setMessages([...messages, ...newMsg])
+  }
   const getTalkingToName = (id) => {
     axios({
       url: 'https://messenger-app.hasura.app/v1/graphql',
@@ -235,14 +238,20 @@ export default function MessageList(props) {
 
         <div className="message-list-container">{renderMessages()}</div>
 
-        <Compose rightItems={[
+        <Compose
+        rightItems={[
           <ToolbarButton key="photo" icon="ion-ios-camera" />,
           <ToolbarButton key="image" icon="ion-ios-image" />,
           <ToolbarButton key="audio" icon="ion-ios-mic" />,
           <ToolbarButton key="money" icon="ion-ios-card" />,
           <ToolbarButton key="games" icon="ion-logo-game-controller-b" />,
           <ToolbarButton key="emoji" icon="ion-ios-happy" />
-        ]}/>
+        ]}
+        idReceiver= {props.receiver}
+        nameReceiver = {talking_to}
+        idSender = {MY_USER_ID}
+        addMessage = {add2Message}
+        />
       </div>
     );
 }
