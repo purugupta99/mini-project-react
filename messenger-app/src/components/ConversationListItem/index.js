c17da2b1d5a7fba02b6648deb84d771e1f0bc6e3
Paritosh Raj <paritoshrajiitd@gmail.com>
2021-06-18 00:45:18 +0530

    Last seen

--- messenger-app/src/components/ConversationListItem/index.js
+++ messenger-app/src/components/ConversationListItem/index.js
@@ -4,6 +4,27 @@ import myInitObject from '../MyInit';
 
 import './ConversationListItem.css';
 
+function timeDifference(date1,date2) {
+  var difference = date1.getTime() - date2.getTime();
+
+  var daysDifference = Math.floor(difference/1000/60/60/24);
+  difference -= daysDifference*1000*60*60*24
+
+  var hoursDifference = Math.floor(difference/1000/60/60);
+  difference -= hoursDifference*1000*60*60
+
+  var minutesDifference = Math.floor(difference/1000/60);
+  difference -= minutesDifference*1000*60
+
+  var secondsDifference = Math.floor(difference/1000);
+
+  console.log('difference = ' + 
+    daysDifference + ' day/s ' + 
+    hoursDifference + ' hour/s ' + 
+    minutesDifference + ' minute/s ' + 
+    secondsDifference + ' second/s ');
+}
+
 export default function ConversationListItem(props) {
   useEffect(() => {
     shave('.conversation-snippet', 20);
@@ -18,12 +39,23 @@ export default function ConversationListItem(props) {
         console.log("Talking to " + talking_to_id);
       }
     }
+    const findDiff = time => {
+      if(time!==null){
+        var now = Date.now();
+        var lastSeen = Date.parse(time);
+        var n = Math.round((now - lastSeen)/60000);
+        return n.toString() + " minutes ago";
+      }
+      else{
+        return "";
+      }
+    }
     return (
       <div className="conversation-list-item" onClick={setMessages(id)}>
         <img className="conversation-photo" src={photo} alt="conversation" />
         <div className="conversation-info">
           <h1 className="conversation-title">{ name }</h1>
-          <p className="conversation-snippet">Last seen: { text }</p>
+          <p className="conversation-snippet">Last seen: { findDiff(text) }</p>
         </div>
       </div>
     );
