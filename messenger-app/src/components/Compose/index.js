407158323414b2a166ff22b1da62c99033bd9d42
Pranshi <pranshiyadav@gmail.com>
2021-06-18 11:56:43 +0530

    compose-add-message (half-working)

--- messenger-app/src/components/Compose/index.js
+++ messenger-app/src/components/Compose/index.js
@@ -80,21 +80,21 @@ const MessageInput = () => {
 
 export default MessageInput;
 
-import React from 'react';
-import './Compose.css';
+// import React from 'react';
+// import './Compose.css';
 
-export default function Compose(props) {
-    return (
-      <div className="compose">
-        <input
-          type="text"
-          className="compose-input"
-          placeholder="Type a message, @name"
-        />
+// export default function Compose(props) {
+//     return (
+//       <div className="compose">
+//         <input
+//           type="text"
+//           className="compose-input"
+//           placeholder="Type a message, @name"
+//         />
 
-        {
-          props.rightItems
-        }
-      </div>
-    );
-}
+//         {
+//           props.rightItems
+//         }
+//       </div>
+//     );
+// }
